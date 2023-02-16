3cbee445a276403e1b315cd0cb5e384f2051728c
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-16 12:16:45 -0800

    Task 4 fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
-int main()
+/**
+ * Display a string using puts
+ */
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
