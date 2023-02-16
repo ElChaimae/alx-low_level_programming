8156ffa5e81deb9b5ad40884ead567d06ba0824e
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-16 12:53:02 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Displays a string
+ *
+ * Return: returns 0 when executed successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
