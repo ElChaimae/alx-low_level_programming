ffb4d947005c9538331de955e0ed6368f2895355
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-17 03:35:22 -0800

    Fix betty style errors on Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: returns 0 when executed successfully
  */
 int main(void)
 {
 	int i;
-	for (i='0'+0;i<'0'+10;i++)
+
+	for (i = '0'+0; i < '0' + 10; i++)
 		putchar(i);
-	putchar('\n');	
+	putchar('\n');
 	return (0);
 }
