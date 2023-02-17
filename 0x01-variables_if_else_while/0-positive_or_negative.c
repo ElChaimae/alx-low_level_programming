0f99e53cabaac6f439ea0fa394f912a07cd33d34
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-17 04:19:02 -0800

    Fix betty style errors on Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Description: The output of the program should be the number, followed by:
  *if the number is greater than 0: is positive
@@ -17,11 +17,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-		printf("%d is positive\n",n);
-	else if (n==0)
-		printf("%d is zero\n",n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 	return (0);
 }
