dd1b2cc681d0ac6c6b6e32f0b5692ed28c7dde40
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-17 04:15:06 -0800

    Fix betty style errors on Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: returns 0 when executed successfully
  */
@@ -13,11 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n%10>5)
-		printf("Last digit of %d is %d and is greater than 5\n",n,n%10);
-	else if(n%10==0)
-		printf("Last digit of %d is %d and is 0\n",n,n%10);
+	if (n % 10 > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,n%10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	return (0);
 }
