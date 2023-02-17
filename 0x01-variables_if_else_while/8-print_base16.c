b2beb1075542f94aa3c7b98507929ae9f1b127b1
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-17 03:25:06 -0800

    Fix betty styles in Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,20 +2,22 @@
 #include<ctype.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: returns 0 when executed successfully
  */
 int main(void)
 {
-	char c[]={0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'};
-	int i;                                                  for (i=0;i<16;i++)
+	char c[]={0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'};
+	int i;
+
+	for (i = 0; i < 16; i++)
 	{
         	if (isalpha(c[i]))
 			putchar(c[i]);
 		else
-			putchar('0'+c[i]);
+			putchar('0' + c[i]);
 	}
-	putchar('\n');	
+	putchar('\n');
 	return (0);
 }
