cca4c7433a53c420ea2c513b7e72ceec68aa3dfa
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-16 21:32:55 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,27 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point 
  *
+ * Description: The output of the program should be the number, followed by:
+ *if the number is greater than 0: is positive
+ *if the number is 0: is zero
+ *if the number is less than 0: is negative followed by a new line
  * Return: returns 0 when executed successfully
  */
 int main(void)
 {
-	
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+		printf("%d is positive\n",n);
+	else if (n==0)
+		printf("%d is zero\n",n);
+	else
+		printf("%d is negative\n",n);
 	return (0);
 }
