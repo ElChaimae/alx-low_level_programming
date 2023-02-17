7ed290e2b26502493ff7700af65b4567cc532ad0
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-17 01:06:11 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<ctype.h>
 
 /**
  * main - Entry point 
@@ -7,6 +8,14 @@
  */
 int main(void)
 {
-	
+	char c[]={0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'};
+	int i;                                                  for (i=0;i<16;i++)
+	{
+        	if (isalpha(c[i]))
+			putchar(c[i]);
+		else
+			putchar('0'+c[i]);
+	}
+	putchar('\n');	
 	return (0);
 }
