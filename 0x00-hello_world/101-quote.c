3686d8afdd46378abec76c5d8bdd989faa91ff87
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-20 03:56:37 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 /*
  * main - prints a string
- * 
+ *
  * Description: Entry point
  * Return: returns 0 to indicate success
  */
@@ -10,5 +10,5 @@
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 0;
+	return (0);
 }
