60aebe5c62e28a732dfcc08eae6b88767af9cb9c
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-21 15:17:09 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-  * tiles_table - prints 9 times tables
+  * times_table - prints 9 times tables
   *
   * Return: void
   */
@@ -10,8 +10,7 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
-	
+	for (i = 0; i < 10; i++)	
 	{
 		printf("0,");
 		for (j = 1; j < 9; j++)
