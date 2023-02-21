1a74df65658a3d2ebe87a7da653e8ab44afdec8e
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-21 12:15:55 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,9 +11,18 @@
 int print_sign(int c)
 {
 	if (c > '0')
+	{
+		_putchar('+');
 		return (1);
+	}
 	else if (c < '0')
+	{
+		_putchar('-');
 		return (-1);
+	}
 	else
+	{
+		_putchar('0');
 		return (0);
+	}
 }
