193a19d541eda794f91199d9591398992e692c09
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-21 09:16:16 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+   * main - This function prints _putchar using putchar
+   *
+   * Return: This function returns 0 on success
+   */
+
+int main(void)
+{
+	char *str = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(str[i]);
+	return (0);
+}
