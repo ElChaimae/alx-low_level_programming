ba5857c889e3fb7948176e1db66d6ddd6234a242
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-21 09:42:13 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+   * main - This function prints _putchar using putchar
+   *
+   * Return: This function returns 0 on success
+   */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 122; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+	return (0);
+}
