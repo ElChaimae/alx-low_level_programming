0980de532386ddbc88de6bb276cf0d7e5c5dff48
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-21 14:51:39 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,11 @@ void times_table(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j= 0; j < 9; j++)
-			printf("%d,  ", i * j);
+		{
+			if (i * j > 9) 
+				printf("%d,  ", i * j);
+			else
+				printf("%d, ", i * j);
 		printf("%d\n", i * j);
 	}
 }
