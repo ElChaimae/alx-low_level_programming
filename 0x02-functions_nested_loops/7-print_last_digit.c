077d442a8d2927ea0217efb47f24397261307f8c
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-21 13:33:16 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * print_last_digit - Prints the last digit of a num
+  * @n: integer
+  *
+  * Return: last digit
+  */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
