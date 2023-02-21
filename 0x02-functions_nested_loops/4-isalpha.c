bd40fbd112a97a7717b4fc26212df497107f1059
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-21 11:52:17 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * isalpha - This function checks for alphabetuc character.
+  * @c: character to be checked
+  *
+  * Return: 1 if c is a letter
+  * 0 otherwise
+  */
+int isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
