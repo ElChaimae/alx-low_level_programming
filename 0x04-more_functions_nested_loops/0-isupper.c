dfb9fa99f60f167da864d19c5427bdf4cd060dce
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-23 04:04:16 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if c is uppercase 
+ * @c: integer
+ *
+ * Description: checks for uppercase letters
+ * Return: return 1 if uppercase
+ * 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
