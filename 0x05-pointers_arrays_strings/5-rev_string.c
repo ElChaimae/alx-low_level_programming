bc31e75a37bfa220afa91016d88dbc402365c3c5
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-27 15:54:58 -0800

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,26 @@
  *
  * Return: void
  */
+
 void rev_string(char *s)
 {
-	int i = _strlen(s);
-	int j = 0;
-	char *s1;
+	int i = 0, j, right;
 
-	i--;
-	for (; i >= 0 ; i--)
+	while (s[i])
+		i++;
+
+	j = i / 2;
+	right = 0;
+
+	while (right != j)
 	{
-		*(s1 + j) = s[i];
-		j++;
+		char temp s[right];
+		int left = i - right - 1;
+
+		s[right] = s[left];
+		s[left] = temp;
+
+		right++;
 	}
 
-	for (j = 0; j < _strlen(s); j++)
-		s[j] = *(s1 + j);
 }
