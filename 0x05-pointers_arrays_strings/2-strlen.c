ad7d7f4bd3ea1d7950a8a9397ede2849f957fd7c
Chaimae <chaimae.el.maataoui@gmail.com>
2023-02-27 11:56:09 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _strlen - calculates the size of a string
+ * @s: pointer to a string
+ *
+ * return : the size of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	return(i);
+}
