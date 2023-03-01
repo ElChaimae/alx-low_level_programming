510554e4526b3ebfc669601f69a5254631d0db27
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-01 13:49:30 -0800

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - it compares two strings.
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ * Return: 0 if equal, -15 or 15 otherwise
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+	if (s1[i] == s2[i])
+		i++;
+	else if (s1[i] > s2[i])
+		return (15);
+	else
+		return (-15);
+}
+	return (0);
+}
