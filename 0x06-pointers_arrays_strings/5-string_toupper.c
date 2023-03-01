756b429ca11185404e42565e54ec161608fbc608
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-01 15:45:25 -0800

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,13 +12,12 @@ char *string_toupper(char *c)
 {
 int i = 0;
 
-while (c[i] != '\n')
+while (c[i] != '\0')
 {
-if (c[i] > 97 && c[i] < 122)
+if (c[i] >= 97 && c[i] <= 122)
 	c[i] = c[i] - 32;
 i++;
 }
 return (c);
 }
-
 
