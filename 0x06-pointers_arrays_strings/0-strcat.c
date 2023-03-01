8f1318d946b0a78256f2c03c1cbe6d29512d39db
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-01 08:39:09 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+int _strlen(char *s);
 
 /**
  * _strcat - concatenates two strings.
@@ -12,11 +13,29 @@
 char *_strcat(char *dest, char *src)
 {
 int i, j;
-int l = strlen(dest);
-for (i = l, j = 0; i < l + strlen(src), j < strlen(src); i++, j++)
+int l = _strlen(dest);
+for (i = l, j = 0; i < l + _strlen(src), j < _strlen(src); i++, j++)
 {
 dest[i] = src[j];
 }
 dest[i] = '\0';
 return (dest);
+}
+
+/**
+ * _strlen - calculates the size of a string
+ * @s: pointer to a string
+ *
+ * Description: takes string and returns its length
+ * Return: the size of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
