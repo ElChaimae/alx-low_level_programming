49fbcd4d96de0e1c91776db2a040c667d0f95323
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-01 14:14:07 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,11 +16,9 @@ while (s1[i] != '\0' && s2[i] != '\0')
 {
 	if (s1[i] == s2[i])
 		i++;
-	else if (s1[i] > s2[i])
-		return (15);
 	else
-		return (-15);
+		return (s1[i] - s2[i]);
 }
-	return (0);
+	return (s1[i] - s2[i]);
 }
 
