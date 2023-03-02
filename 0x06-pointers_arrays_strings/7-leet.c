0364ac9ca02934eb1c0329a8013e3da3d193f8ad
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-01 17:38:59 -0800

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string
+ *
+ * Return: string
+ */
+
+char *leet(char *s)
+{
+int i = 0;
+
+while (s[i] != '\n')
+{
+int j;
+char a[][2] = {{'a', '4'}, {'A', '4'},
+	{'e', '3'}, {'E', '3'}, {'o', '0'},
+	{'O', '0'}, {'t', '7'}, {'T', '7'},
+	{'l', '1'}, {'L', '1'}};
+
+for (j = 0; j < 10; j++)
+{
+	if (a[j][0] == s[i])
+		s[i] = a[j][1];
+
+}
+i++;
+}
+return (s);
+}
+
