55f3392510601d5d8634d97d83da660431db22c6
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-02 12:17:27 -0800

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,12 @@
  */
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	if (n > 0)
+	if (n >= 0)
 	{
 		if (n / 10)
 			print_number(n / 10);
