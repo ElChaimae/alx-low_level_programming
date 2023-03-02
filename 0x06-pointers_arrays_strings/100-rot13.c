866ad0f87d4b50c74c77bda70a797ac83059eab1
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-02 05:43:38 -0800

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rot13 - it encodes a string using rot13.
+ * @s: string to be encoded
+ *
+ * Return: string.
+ */
+char *rot13(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+	if ((s[i] >= 'a' && s[i] <= 'm')
+	|| (s[i] >= 'A' && s[i] <= 'M'))
+		s[i] += 13;
+	else if ((s[i] >= 'n' && s[i] <= 'z')
+	|| (s[i] >= 'N' && s[i] <= 'Z'))
+		s[i] -= 13;
+	i++;
+}
+return (s);
+}
