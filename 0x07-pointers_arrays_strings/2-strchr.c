9838e62f8861205fad3bd9ee0e0280fe47ed38fc
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-06 07:16:16 -0800

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to be located
+ *
+ * Return: character pointer or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (&s[i]);
+		i++;
+	}
+	return (NULL);
+}
