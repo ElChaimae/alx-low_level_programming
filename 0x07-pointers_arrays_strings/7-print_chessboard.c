64f8a554d1eb8ac72793d57d3fafc1855298c6dc
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-06 12:39:22 -0800

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chess board
+ * @a: pointer to a 2E array
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			putchar(a[i][j]);
+			j++;
+		}
+		putchar('\n');
+	}
+
+}
