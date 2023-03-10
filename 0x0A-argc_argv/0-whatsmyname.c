206a93b251ae525880519798db53f09b3f6b6e43
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-10 08:15:45 -0800

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ * @argc: Length of argv
+ * @argv: Arrays of program arguments
+ *
+ * Return: 0, Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
