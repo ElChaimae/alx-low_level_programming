db4ac9bd6f66ba4e5c9588c99ab4be3110b535b1
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-13 13:52:22 -0700

    Task0 fixes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	str = (char *)malloc(sizeof(char) * size);
+	str = malloc(sizeof(char) * size);
 
 	while ((int) size >= 0)
 	{
