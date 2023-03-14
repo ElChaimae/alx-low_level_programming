7e4990c089cca9503f7b7a474d12f8983fe8b3b2
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-13 18:50:06 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,19 +12,26 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, l;
+	int i = 0, j = 0, l = 0;
 	char *s;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 != NULL)
+	{
 	for (i = 0; s1[i] != '\0'; i++)
 		;
+	}
+
+	if (s2 != NULL)
+	{
 	for (j = 0; s2[j] != '\0'; j++)
 		;
+	}
 	l = i + j;
 	s = malloc(sizeof(*s) * (l + 1));
 	if (s == NULL)
 		return (NULL);
+	if (s1 == NULL && s2 == NULL)
+		s[0] = '\0';
 	if (s1 != NULL)
 	{
 	while (i >= 0)
