b6eba7f8c637cdbc5136dbf6ebb9c2d978529fd2
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-20 03:48:51 -0700

    Task 1

--- 0x0E-structures_typedef/1main.c
+++ 0x0E-structures_typedef/1main.c
@@ -9,8 +9,10 @@
 int main(void)
 {
     struct dog my_dog;
+    char str[10];
 
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    *str = '\0';
+    init_dog(&my_dog, str, 3.5, "Bob");
     printf("My name is %s, and I am %.1f and my owner is %s :) - Woof!\n", my_dog.name, my_dog.age, my_dog.owner);
     return (0);
 }
