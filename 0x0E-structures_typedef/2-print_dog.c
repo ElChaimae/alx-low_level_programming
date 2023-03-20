6a0c68d37d937aaf2a9ef0434e68ce5b59cf60d7
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-20 10:34:14 -0700

    Fix task 2 errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,12 +1,11 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-/*
- * print_dog - it prints a struct dog
- * @d: struct dog
- *
- */
 
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
