f1a71f3d1e50e6d22e2cafe2cc97dc570dd090bf
Chaimae <chaimae.el.maataoui@gmail.com>
2023-03-22 05:39:18 -0700

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - call the action function on elements of an array
+ * @array: array of parameters
+ * @size: array size
+ * @action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (action && (array + i))
+			action(*(array + i));
+	}
+}
