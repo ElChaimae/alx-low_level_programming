e9beb6bc501500eea0ee969f3227002f8b3cc599
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-04-02 02:47:25 -0700

    Fix errors on 101 task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,23 @@
 section .data
    msg: db "Hello, Holberton", 10
+   fmt: db "%s", 10, 0
+
+section .text
+   global main
+
+extern printf
+
+main:
+   push rbp
+   mov rdi,fmt
+   mov rsi,msg
+   xor rax,rax
+   call printf
+   pop rbp
+   xor rax,rax
+   ret
+section .data
+   msg: db "Hello, Holberton", 10
 
 section .text
    global _start
