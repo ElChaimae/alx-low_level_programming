3e6180cb5c0e8dcc1307668b547782fd364354fc
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-04-02 03:01:13 -0700

    Fix 101 task errors

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,35 +1,11 @@
-section .data
-   msg: db "Hello, Holberton", 10
-   fmt: db "%s", 10, 0
-
-section .text
-   global main
-
 extern printf
-
-main:
-   push rbp
-   mov rdi,fmt
-   mov rsi,msg
-   xor rax,rax
-   call printf
-   pop rbp
-   xor rax,rax
-   ret
 section .data
-   msg: db "Hello, Holberton", 10
-
+	format: db "Hello, Holberton", 10, 0
 section .text
-   global _start
-
-_start:
-   mov eax, 1
-   mov ebx, 1
-   mov ecx, msg
-   mov edx, 17
-   int 0x80
-
-   mov eax, 1
-   xor ebx, ebx
-   int 0x80
-
+global main
+main:
+	mov rdi, format
+	xor eax, eax
+	call printf
+	mov eax, 0
+	ret
