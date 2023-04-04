3004d9b2eea4a09b59babaab6ee9ab603d495ee0
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-04-04 13:53:16 -0700

    Fix Task 9 errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -58,7 +58,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	if ((*head)->next == NULL)
-		return (NULL);
+
 
 	return (insert_nodeint_at_index(&((*head)->next), --idx, n));
 }
