e8a802712741c94ec80000f43f49fda0b3bbeba2
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-04-04 17:11:06 -0700

    start over Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,27 +2,6 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint - Adds a new node at the beginning of a listint_t list.
- * @head: Pointer to a pointer to the head node of the list.
- * @n: Value of the integer to be added to the new node.
- *
- * Return: The address of the new element, or NULL if it failed.
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *node;
-
-	node = malloc(sizeof(*node));
-	if (node)
-	{
-		node->n = n;
-		node->next = *head;
-		*head = node;
-	}
-	return (node);
-}
-
-/**
  * insert_nodeint_at_index - Inserts a new node
  * at a given position in a linked list.
  * @head: Pointer to a pointer to the first node of the list.
@@ -35,30 +14,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node;
 
-	if (idx != 0 && (head == NULL || *head == NULL || (*head)->next == NULL))
-		return (NULL);
-
-	if (idx == 0 && (head == NULL || *head == NULL))
+	if (idx == 0)
 	{
-		node = add_nodeint(head, n);
-		return (node);
-	}
-
-	if (idx == 1)
-	{
-		node = malloc(sizeof(*node));
+		node = malloc(sizeof(listint_t));
 		if (node == NULL)
 			return (NULL);
 
 		node->n = n;
-		node->next = (*head)->next;
-		(*head)->next = node;
+		node->next = *head;
+		*head = node;
 
 		return (node);
 	}
 
-	if ((*head)->next == NULL)
+	if (*head == NULL)
+		return (NULL);
 
-
-	return (insert_nodeint_at_index(&((*head)->next), --idx, n));
+	return (insert_nodeint_at_index(&((*head)->next), idx - 1, n));
 }
