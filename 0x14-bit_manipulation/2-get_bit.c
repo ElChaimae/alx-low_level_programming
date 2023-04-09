6ddbdc818af75548b8496111c1c5118ea8b0569c
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-04-09 00:00:45 -0700

    Handle case where index is greater than number of bits

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,24 @@
 #include "main.h"
+
+/**
+ * count_bits - Returns the number of bits in a decimal number.
+ * @num: The decimal number to count the bits of.
+ *
+ * Return: The number of bits in the decimal number.
+ */
+unsigned int count_bits(unsigned long int num)
+{
+	unsigned int count;
+
+	count = 0;
+	while (num > 0)
+	{
+		num >>= 1;
+		count++;
+	}
+	return (count - 1);
+}
+
 /**
  * get_bit - This function returns the value of a bit at a given index.
  *
@@ -11,6 +31,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int mask;
 
+	if (count_bits(n) < index)
+		return (-1);
 	mask = 1 << index;
 	if (n & mask)
 		return (1);
