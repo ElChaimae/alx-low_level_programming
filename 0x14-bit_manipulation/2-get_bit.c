ebdb44344cd4823fa8c5a720d85d56cf667c3fc8
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-04-09 00:27:29 -0700

    Fix error on Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * count_bits - Returns the number of bits in a decimal number.
- * @num: The decimal number to count the bits of.
- *
- * Return: The number of bits in the decimal number.
- */
-unsigned int count_bits(unsigned long int num)
-{
-	unsigned int count;
-
-	count = 0;
-	while (num > 0)
-	{
-		num >>= 1;
-		count++;
-	}
-	return (count - 1);
-}
-
-/**
  * get_bit - This function returns the value of a bit at a given index.
  *
  * @n: The number to extract the bit from.
@@ -31,7 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int mask;
 
-	if (count_bits(n) < index)
+	if (index > sizeof(unsigned long int) * 8 - 1)
 		return (-1);
 	mask = 1 << index;
 	if (n & mask)
