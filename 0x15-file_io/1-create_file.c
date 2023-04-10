2125c754f5a32d6d3a4e938e1b5ba2cc53759e8e
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-04-10 13:52:00 -0700

    Correct Return statement in Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,5 +25,5 @@ int create_file(const char *filename, char *text_content)
 	if (fildes < 0 || bytes_written < 0 || bytes_written != count)
 		return (-1);
 	close(fildes);
-	return (0);
+	return (1);
 }
