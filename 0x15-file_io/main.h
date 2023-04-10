9fbe5caf8b6adadb891736e611e189271e5b6dfe
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-04-10 10:19:27 -0700

    update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
