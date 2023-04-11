94726626b6fd55d419343608d4442319d74fc090
ElChaimae <chaimae.el.maataoui@gmail.com>
2023-04-11 02:21:37 -0700

    Fix failing tests at task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (!text_content)
+		return (1);
 	fildes = open(filename, O_WRONLY | O_APPEND);
 	if (fildes < 0 || !text_content)
 		return (-1);
